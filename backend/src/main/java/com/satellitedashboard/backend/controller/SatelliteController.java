a0bec46dc939cf84e930716272ff6ba86003b683
hamzapaulpro <hamza.paul.pro@gmail.com>
2026-08-02 07:58:55 +0200

    wire to frondend

--- backend/src/main/java/com/satellitedashboard/backend/controller/SatelliteController.java
+++ backend/src/main/java/com/satellitedashboard/backend/controller/SatelliteController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/satellites")
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class SatelliteController {
 
     private final SatelliteService satelliteService;
